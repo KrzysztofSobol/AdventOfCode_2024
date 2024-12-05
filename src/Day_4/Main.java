6594bde4eccdf9dc25979cc960ca8f5f362b1928
Kex <krzysieksobol9@gmail.com>
2024-12-05 13:04:14 +0100

    Day_4 done, crazy array flips

--- src/Day_4/Main.java
+++ src/Day_4/Main.java
@@ -4,10 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Main {
-    public static ArrayList<String> diagonalArrayLR(char[][] data, int cols){
+    private static ArrayList<String> diagonalArrayLR(char[][] data, int cols){
         ArrayList<String> diagonalStrings = new ArrayList<>();
         for (int i = 0; i < cols; i++) {
             StringBuilder diagonal = new StringBuilder();
@@ -31,7 +30,7 @@ public class Main {
         return diagonalStrings;
     }
 
-    public static ArrayList<String> diagonalArrayRL(char[][] data, int cols){
+    private static ArrayList<String> diagonalArrayRL(char[][] data, int cols){
         ArrayList<String> diagonalStrings = new ArrayList<>();
         for (int i = 0; i < cols; i++) {
             StringBuilder diagonal = new StringBuilder();
@@ -51,7 +50,7 @@ public class Main {
         return diagonalStrings;
     }
 
-    public static int countXMASOccurrences(String[] data) {
+    private static int countXMASOccurrences(String[] data) {
         int totalOccurrences = 0;
 
         for (String str : data) {
@@ -71,6 +70,40 @@ public class Main {
         }
         return count;
     }
+
+    private static boolean checkX_MASPattern(char tl, char tr, char bl, char br){
+        if(tl == 'M'){
+            if(tr == 'M' && bl == 'S' && br == 'S'){
+                return true;
+            } else if(tr == 'S' && bl == 'M' && br == 'S'){
+                return true;
+            }
+        } else if(tl == 'S'){
+            if(tr == 'S' && bl == 'M' && br == 'M'){
+                return true;
+            } else if(tr == 'M' && bl == 'S' && br == 'M'){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int countX_MASOccurrences(char[][] data, int cols) {
+        int count = 0;
+
+        for (int i = 1; i < cols-1; i++) {
+            for (int j = 1; j < cols-1; j++) {
+                if(data[i][j] == 'A'){
+                    if(checkX_MASPattern(data[i-1][j-1], data[i-1][j+1], data[i+1][j-1], data[i+1][j+1])){
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+
 
     public static void main(String[] args) throws IOException {
         String filePath = "src/Day_4/data.txt";
@@ -95,31 +128,33 @@ public class Main {
             }
         }
 
-        String[] transposedStringArray1 = new String[numberOfColumns];
-        String[] transposedStringArray2 = new String[numberOfColumns];
+        String[] originalStringArray = new String[numberOfColumns];
+        String[] transposedStringArray = new String[numberOfColumns];
         String[] diagonalArrayLR = diagonalArrayLR(originalDataArray, numberOfColumns).toArray(new String[0]);
         String[] diagonalArrayRL = diagonalArrayRL(originalDataArray, numberOfColumns).toArray(new String[0]);
 
         // original array
         for (int i = 0; i < numberOfColumns; i++) {
-            transposedStringArray1[i] = new String(originalDataArray[i]);
+            originalStringArray[i] = new String(originalDataArray[i]);
         }
 
         // transposed array
         for (int i = 0; i < numberOfColumns; i++) {
-            transposedStringArray2[i] = new String(transposedData[i]);
+            transposedStringArray[i] = new String(transposedData[i]);
         }
 
-        totalXMAS += countXMASOccurrences(transposedStringArray1);
-        totalXMAS += countXMASOccurrences(transposedStringArray2);
+        totalXMAS += countXMASOccurrences(originalStringArray);
+        totalXMAS += countXMASOccurrences(transposedStringArray);
         totalXMAS += countXMASOccurrences(diagonalArrayLR);
         totalXMAS += countXMASOccurrences(diagonalArrayRL);
 
-        System.out.println("Count in Transposed1: " + countXMASOccurrences(transposedStringArray1));
-        System.out.println("Count in Transposed2: " + countXMASOccurrences(transposedStringArray2));
+        System.out.println("Count in Transposed1: " + countXMASOccurrences(originalStringArray));
+        System.out.println("Count in Transposed2: " + countXMASOccurrences(transposedStringArray));
         System.out.println("Count in Diagonal LR: " + countXMASOccurrences(diagonalArrayLR));
         System.out.println("Count in Diagonal RL: " + countXMASOccurrences(diagonalArrayRL));
 
         System.out.println("Total XMAS: " + totalXMAS);
+
+        System.out.println("Total X-MAS patterns: " + countX_MASOccurrences(originalDataArray, numberOfColumns));
     }
 }
