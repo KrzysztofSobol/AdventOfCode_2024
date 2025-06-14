33c925a584a0058c3fb9d02757a32ddf0bcce3fe
KrzysztofSobol <krzysieksobol9@gmail.com>
2025-06-14 16:47:49 +0200

    Day_9 part 2 done

--- Day_9/Day_9/Main.java
+++ Day_9/Day_9/Main.java
@@ -8,6 +8,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    public static int blockLength(List<Integer> disk, int index){
+        int num = disk.get(index);
+        for(int i=index-1; i>=0; i--){
+            if(disk.get(i) != num || i==0){
+                return index - i;
+            }
+        }
+        return -1;
+    }
+
+    public static void insertBlock(List<Integer> disk, int blockLength, int blockIndex){
+        int count = 0;
+        int blockId = disk.get(blockIndex);
+        for(int i=0; i < blockIndex; i++){
+            if(disk.get(i) == -1){
+                count++;
+            } else {
+                count = 0;
+            }
+
+            if(count == blockLength){
+                int startIndex = i-count+1;
+                for(int j = 0; j<count; j++){
+                    disk.set(startIndex+j, blockId);
+                    disk.set(j+blockIndex, -1);
+                }
+                break;
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         final String filePath = "Day_9/data.txt";
         String diskMap = Files.readString(Paths.get(filePath));
@@ -30,31 +61,30 @@ public class Main {
             }
         }
 
-        int left = 0, right = decodedDisk.size()-1;
-        while(decodedDisk.get(left) != -1){ left++; }
-        while(decodedDisk.get(right) == -1){ right--; }
-
         // move the file blocks
-        while(left <= right){
-            decodedDisk.set(left, decodedDisk.get(right));
-            decodedDisk.set(right, -1);
+        int right = decodedDisk.size()-1;
+        int blockLength;
 
-            while(decodedDisk.get(left) != -1){ left++; }
-            while(decodedDisk.get(right) == -1){ right--; }
+        while (right > 0){
+            blockLength = blockLength(decodedDisk, right);
+            insertBlock(decodedDisk, blockLength, right-blockLength+1);
+            right -= blockLength;
+
+            while (decodedDisk.get(right) == -1){
+                right--;
+            }
         }
 
         // filesystem checksum
         BigInteger checksum = BigInteger.ZERO;
         for(int i=0; i<decodedDisk.size(); i++){
-            if(decodedDisk.get(i) == -1){
-                break;
+            if(decodedDisk.get(i) != -1){
+                BigInteger block = BigInteger.valueOf(decodedDisk.get(i));
+                BigInteger fileId = BigInteger.valueOf(i);
+                BigInteger outcome = block.multiply(fileId);
+
+                checksum = checksum.add(outcome);
             }
-
-            BigInteger block = BigInteger.valueOf(decodedDisk.get(i));
-            BigInteger fileId = BigInteger.valueOf(i);
-            BigInteger outcome = block.multiply(fileId);
-
-            checksum = checksum.add(outcome);
         }
 
         System.out.println(checksum);
