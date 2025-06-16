56712ae254555dc03fff991401504feb9cce391f
KrzysztofSobol <krzysieksobol9@gmail.com>
2025-06-16 19:08:41 +0200

    Day_11 part 2 done (My first version would take a billion years)

--- Day_11/Day_11/Main.java
+++ Day_11/Day_11/Main.java
@@ -7,6 +7,8 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class Main {
+    private static Map<String, Long> cache = new HashMap<>();
+
     public static String reduceString(String s){
         int index = 0, i;
 
@@ -24,52 +26,34 @@ public class Main {
         return s.substring(index);
     }
 
-    private static void blink(List<String> list, int times) {
-        record Split (String left, String right) {}
-        HashMap<String, String> cache = new HashMap<>();
-        HashMap<String, Split> cacheSplit = new HashMap<>();
-
-        for(int i=0; i<times; i++){
-            ListIterator<String> iter = list.listIterator();
-
-            while(iter.hasNext()){
-                String current = iter.next();
-
-                if(Objects.equals(current, "0")) {   // 0 swap rule
-                    iter.set("1");
-                } else if(current.length()%2 == 0){      // split rule
-                    if(cacheSplit.containsKey(current)){
-                        System.out.println("SplitCache used for: " + current);
-                        Split split = cacheSplit.get(current);
-                        iter.set(split.left);
-                        iter.add(split.right);
-                    } else {
-                        int mid = current.length() / 2;
-                        String firstHalf = current.substring(0, mid);
-                        String secondHalfPre = current.substring(mid);
-                        String secondHalf = reduceString(secondHalfPre);
-
-                        iter.set(firstHalf);
-                        iter.add(secondHalf);
-
-                        cacheSplit.put(current, new Split(firstHalf, secondHalf));
-                    }
-                } else {                                  // times 2024 rule
-                    if(cache.containsKey(current)){ // cache check
-                        System.out.println("Cache used for: " + current);
-                        iter.set(cache.get(current));
-                    } else {
-                        BigInteger number = BigInteger.valueOf(Integer.parseInt(current));
-                        BigInteger multipliedNumber = number.multiply(BigInteger.valueOf(2024L));
-                        String result = multipliedNumber.toString();
-
-                        cache.put(current, result);
-                        iter.set(result);
-                    }
-                }
-            }
-            System.out.println("Blink number: " + (1+i) + " finished! List size is: " + list.size());
+    private static long blink(String stone, int times) {
+        if(times == 0){
+            return 1;
         }
+
+        String key = stone + "," + times;
+        if(cache.containsKey(key)){
+            return cache.get(key);
+        }
+
+        long result;
+
+        if(stone.equals("0")) {
+            result = blink("1", times-1);
+        } else if(stone.length()%2 == 0) {
+           int mid = stone.length() / 2;
+           String left = stone.substring(0, mid);
+           String right = reduceString(stone.substring(mid));
+
+           result = blink(left, times-1) + blink(right, times+-1);
+        } else {
+            BigInteger number = new BigInteger(stone);
+            BigInteger multiplied = number.multiply(BigInteger.valueOf(2024L));
+            result = blink(multiplied.toString(), times-1);
+        }
+
+        cache.put(key, result);
+        return result;
     }
     
 
@@ -79,11 +63,13 @@ public class Main {
 
         // load data in
        String data = Files.readString(Paths.get(filePath));
-       String[] numbers = data.split(" ");
-       List<String> rocks = new LinkedList<>(Arrays.asList(numbers));
+       String[] stones = data.split(" ");
 
-       blink(rocks, NUMBER_OF_ITERATIONS);
+       long stoneTotal = 0;
+       for(String stone : stones){
+           stoneTotal += blink(stone, NUMBER_OF_ITERATIONS);
+       }
 
-       System.out.println("Number of stones: " + rocks.size() + " after " + NUMBER_OF_ITERATIONS + " iterations");
+       System.out.println("Number of stones: " + stoneTotal + " after " + NUMBER_OF_ITERATIONS + " iterations");
     }
 }
