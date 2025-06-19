cc8b8aaa2731f337b20acf87ed3844ebfb226768
KrzysztofSobol <krzysieksobol9@gmail.com>
2025-06-19 12:57:42 +0200

    Day_12 part 1 done

--- Day_12/Day_12/Main.java
+++ Day_12/Day_12/Main.java
@@ -3,56 +3,73 @@ package Day_12;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
 public class Main {
+    static HashSet<String> visited = new HashSet<>();
     static int ROWS = 0;
     static int COLS = 0;
 
-    public static int getFences(char[][] grid, char type, int x, int y){
+
+    public static int gardenScan(char[][] grid, int x, int y){
+        visited.add(x + "," + y);
+        char type = grid[x][y];
         int fencesTotal = 0;
 
         if(y+1 < COLS){
-            if(grid[x][y+1] != type){
-                fencesTotal++;
+            if(!visited.contains(x + "," + (y+1)) && grid[x][y+1] == type){
+                fencesTotal += gardenScan(grid, x, y+1);
+            } else {
+                if(grid[x][y+1] != type){
+                    fencesTotal++;
+                }
             }
         } else {
             fencesTotal++;
         }
 
         if(x+1 < ROWS){
-            if(grid[x+1][y] != type){
-                fencesTotal++;
+            if(!visited.contains((x+1) + "," + y) && grid[x+1][y] == type){
+                fencesTotal += gardenScan(grid, x+1, y);
+            } else {
+                if(grid[x+1][y] != type){
+                    fencesTotal++;
+                }
             }
         } else {
             fencesTotal++;
         }
 
         if(y-1 >= 0){
-            if(grid[x][y-1] != type){
-                fencesTotal++;
+            if(!visited.contains(x + "," + (y-1)) && grid[x][y-1] == type){
+                fencesTotal += gardenScan(grid, x, y-1);
+            } else {
+                if(grid[x][y-1] != type){
+                    fencesTotal++;
+                }
             }
         } else {
             fencesTotal++;
         }
 
         if(x-1 >= 0){
-            if(grid[x-1][y] != type){
-                fencesTotal++;
+            if(!visited.contains((x-1) + "," + y) && grid[x-1][y] == type){
+                fencesTotal += gardenScan(grid, x-1, y);
+            } else {
+                if(grid[x-1][y] != type){
+                    fencesTotal++;
+                }
             }
         } else {
             fencesTotal++;
         }
-        System.out.println(fencesTotal + "  :  " + x + " " + y);
 
         return fencesTotal;
     }
 
     public static void main(String[] args) throws IOException {
         final String filePath = "Day_12/data.txt";
-        HashMap<Character, Integer> map = new HashMap<>();
 
         // load data in
         List<String> data = Files.readAllLines(Paths.get(filePath));
@@ -68,23 +85,26 @@ public class Main {
         }
 
         // count the fences
+        int lastVisitedSize = 0;
+        int sumTotal = 0;
+
         for(int i=0; i<ROWS; i++){
             for(int j=0; j<COLS; j++){
                 char c = grid[i][j];
-                int currentCount = map.computeIfAbsent(c, k -> 0);
-                currentCount += getFences(grid, c, i, j);
-                map.put(c, currentCount);
+                if(!visited.contains(i + "," +j)){
+
+                    int perimeter = gardenScan(grid, i, j);
+
+                    int visitedSize = visited.size();
+                    int region = visitedSize - lastVisitedSize;
+                    System.out.println(region + " * " + perimeter);
+                    sumTotal += (region * perimeter);
+
+                    lastVisitedSize = visitedSize;
+                }
             }
         }
 
-        int sumTotal = map.values().stream().mapToInt(Integer::intValue).sum();
         System.out.println(sumTotal);
-
-        for(int i=0; i<ROWS; i++){
-            for(int j=0; j<COLS; j++){
-                System.out.print(grid[i][j]);
-            }
-            System.out.println();
-        }
     }
 }
