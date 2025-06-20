0e0bc38a4613b296313cb69fabf475c206388bde
KrzysztofSobol <krzysieksobol9@gmail.com>
2025-06-20 14:48:45 +0200

    Day_13 init

---
+++ Day_13/Day_13/Main.java
@@ -0,0 +1,12 @@
+package Day_13;
+
+class Main {
+    public static int gcd(int a, int b){
+        if(b == 0) return a;
+        return gcd(b, a%b);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
